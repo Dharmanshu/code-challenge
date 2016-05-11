ecb7663407be1934e8b9337ab5020d2481a5170a
Dharmanshu <dharmit88@gmail.com>
2016-05-11 15:12:40 -0400

    Nav bar with accessibility role and basic section content

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var fs = require('file-system');
-
-var index = fs.readFileSync('./src/index.html');
-
-http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end(index);
-}).listen(3000);
-
-
-console.log('server is now running');
+// var http = require('http');
+// var fs = require('file-system');
+//
+// var index = fs.readFileSync('./src/index.html');
+//
+// http.createServer(function (req, res) {
+//     res.writeHead(200);
+//     res.end(index);
+// }).listen(3000);
+//
+//
+// console.log('server is now running');
