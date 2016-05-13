83eb6e73fead1dad1fe1c4d383d33519b0e838f3
Dharmanshu <dharmit88@gmail.com>
2016-05-13 01:46:45 -0400

    Moved logic to append rows into table from JSON  data into table.js

---
+++ src/common/table.js
@@ -0,0 +1,25 @@
+"use strict"
+
+$(document).ready(function () {
+
+    $.getJSON("https://api.myjson.com/bins/11h4i",
+        function (data) {
+            var rowList = [];
+
+            data.sort(function (a, b) {
+                return parseFloat(b.earnings - parseFloat(a.earnings));
+            });
+
+            data.forEach(function (data) {
+                var row;
+                row = ['<tr>',
+                    '<td>' + data.name + '</td>',
+                    '<td class="text-right">' + data.apy + '</td>',
+                    '<td class="text-right">' + data.earnings + '</td>',
+                    '</tr>'].join('');
+                rowList.push(row);
+            });
+
+            $('table').find('[data-js="check-rate-table"]').empty().append(rowList.join(''));
+        });
+});
